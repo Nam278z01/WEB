58799f87a975506ae4c424337ab8d76f1a69206e
Nam Nguyễn <nam278z01@gmail.com>
2021-04-16 01:35:45 +0700

    responsive menu

--- Movie/assets/js/main.js
+++ Movie/assets/js/main.js
@@ -1,10 +1,23 @@
 window.addEventListener('scroll', function (e) {
     var nav = document.getElementById('mynav');
     if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
-            nav.classList.add('nav-colored');
-            nav.classList.remove('nav-transparent');
-        } else {
-            nav.classList.add('nav-transparent');
-            nav.classList.remove('nav-colored');
-        }
+        nav.classList.add('nav-colored');
+        nav.classList.remove('nav-transparent');
+    }
+    else {
+        nav.classList.add('nav-transparent');
+        nav.classList.remove('nav-colored');
+    }
 });
+$("#icon-search").click(function () {
+    $("#search-wrap").css("display", "flex");
+});
+$("#search-tnof").click(function () {
+    $("#search-wrap").css("display", "none");
+});
+window.onclick = function (event) {
+    var search = document.getElementById("search-wrap");
+    if (event.target == search) {
+        search.style.display = "none";
+    }
+}
