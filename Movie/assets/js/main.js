5987409ffcb95017547bdc79a3b31630b6074adc
Nam Nguyễn <nam278z01@gmail.com>
2021-04-18 15:07:00 +0700

    tắt hover

--- Movie/assets/js/main.js
+++ Movie/assets/js/main.js
@@ -63,69 +63,85 @@ $(document).ready(function () {
         $("#search-wrap").hide();
     });
 
-    // var margin_left = 25;
-    // $(".row__item-next").click(function () {
-    //     if (margin_left <= 25 && margin_left > -200) {
-    //         margin_left -= $(window).width();
-    //         $("#row__img-first").css("margin-left", margin_left.toString() + "px");
-    //     }
-    // });
-    // $(".row__item-back").click(function () {
-    //     if (margin_left < 25) {
-    //         margin_left += $(window).width();
-    //         $("#row__img-first").css("margin-left", margin_left.toString() + "px");
-    //     }
-    // });
-
-    // Hover film
-    var myTimeout;
-    var parent;
-    var child;
-    $(".row__img-link").mouseenter(function (e) {
-        console.log([$(this)]);
-        var left = $(this).offset().left + 'px';
-        // var top = $(this).offset().top + 'px';
-        // alert(top);
-        parent = $(this).parent();
-        child = parent.html();
-        myTimeout = setTimeout(function () {
-            var addChild = `<div class="hover-movie" id="hover">
-        <a href="" class="hover-movie-link">
-            <img src="./assets/img/image1.jpg" alt="" class="hover-movie__img">
-            <video autoplay muted id="hover-movie__video">
-                <source src="./assets/video/armyofthedead.mp4" type="video/mp4">
-            </video>
-        </a>
-        <img src="./assets/img/image_name1.png" alt="" class="hover-movie__video-name">
-        <div class="hover-movie__button">
-            <button class="button hover-movie__button--play" title="Xem ngay Daredevil">
-                <i class="fas fa-play"></i>
-                <span>Xem ngay</span>
-            </button>
-            <button class="button hover-movie__button--addlist" title="Thêm vào danh sách của tôi">
-                <i class="fas fa-plus"></i>
-                <span>Danh sách</span>
-            </button>
-            <button class="button hover-movie__button--moreinfo" href="#" title="Chi tiết Daredevil">
-                <i class="fas fa-info-circle"></i>
-                <span>Chi tiết</span>
-            </button>
-        </div>
-    </div>`;
-            parent.after(addChild);
-            $("#hover").css("top", "-30px");
-            $("#hover").css("left", left);
-        }, 700);
-    }).mouseleave(function () {
-        clearTimeout(myTimeout);
-        $("#hover").mouseleave(function () {
-            clearTimeout(myTimeout);
-            $(this).remove();
-        });
-        
-    })
-
+    //Slider
+    var margin_left = $(window).width() * 3 / 100;
+    $(".row__item-next").click(function () {
+        var parent = $(this).parent();
+        var slide = $(this).parent().children(".row__img-link:nth-child(3)");
+        if (margin_left <= 56 && margin_left > - 2400) {
+            margin_left -= (213) * 2;
+            slide.css("margin-left", margin_left.toString() + "px");
+            
+        }
+    });
     
+    // var back = parent.children(".row__item-back").show();
+    // if (margin_left > 56) {
+    //     back.click(function () {
+    //         var back = $(this).parent().children(".row__img-link:nth-child(3)");
+    //         console.log(next);
+    //         margin_left -= (213) * 2;
+    //         slide.css("margin-left", margin_left.toString() + "px");
+
+    //     })
+    // }
+    $(window).on("load resize", function (e) {
+        if (document.documentElement.clientWidth > 1024) {
+            var myTimeout;
+            var parent;
+            var child;
+            $(".row__img-link").mouseenter(function (e) {
+                console.log([$(this)]);
+                var left = $(this).offset().left + 'px';
+                // var top = $(this).offset().top + 'px';
+                // alert(top);
+                parent = $(this).parent();
+                child = parent.html();
+                myTimeout = setTimeout(function () {
+                    var addChild = `<div class="hover-movie" id="hover">
+                <a href="#" class="hover-movie-link">
+                    <img src="./assets/img/image1.jpg" alt="" class="hover-movie__img">
+                    <video id="hover-movie__video">
+                        <source src="./assets/video/armyofthedead.mp4" type="video/mp4">
+                    </video>
+                </a>
+                <img src="./assets/img/image_name1.png" alt="" class="hover-movie__video-name">
+                <div class="hover-movie__button">
+                    <button class="button hover-movie__button--play" title="Xem ngay Daredevil">
+                        <i class="fas fa-play"></i>
+                        <span>Xem ngay</span>
+                    </button>
+                    <button class="button hover-movie__button--addlist" title="Thêm vào danh sách của tôi">
+                        <i class="fas fa-plus"></i>
+                        <span>Danh sách</span>
+                    </button>
+                    <button class="button hover-movie__button--moreinfo" href="#" title="Chi tiết Daredevil">
+                        <i class="fas fa-info-circle"></i>
+                        <span>Chi tiết</span>
+                    </button>
+                </div>
+            </div>`;
+                    parent.after(addChild);
+                    $("#hover").css("top", "-30px");
+                    $("#hover").css("left", left);
+                    $("#myVideo").get(0).pause();
+                    $("#hover-movie__video").get(0).currentTime = 0;
+                    setTimeout(function () {
+                        $(".hover-movie__img").hide();
+                        $("#hover-movie__video").show();
+                        $("#hover-movie__video").get(0).play();
+                    }, 3000);
+                }, 700);
+            }).mouseleave(function () {
+                clearTimeout(myTimeout);
+                $("#hover").mouseleave(function () {
+                    clearTimeout(myTimeout);
+                    $(this).remove();
+                });
+            })
+        };
+    });
+    // Hover film
 });
 
 
