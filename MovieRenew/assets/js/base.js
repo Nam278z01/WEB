7afd3875dd2a5dc1d2773c7f471d5f007980f1f6
Nam Nguyễn <nam278z01@gmail.com>
2021-07-10 00:27:32 +0700

    slide trượt

--- MovieRenew/assets/js/base.js
+++ MovieRenew/assets/js/base.js
@@ -1,20 +1,33 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-let btnMenu = $('#sidebar .sidebar__brand i')
-let sidebar = $("#sidebar")
-let btnSlideMovie = $$('.slide-pick__item')
-let slide = $('.content__slide-list')
-let slideEntire = $('.content__slide')
 let header = $('header')
-let inputSearch = $('.search__container input')
-let btnSearch = $('#header__search-form .btn')
+let sidebar = $("#sidebar")
+let btnMenu = sidebar.querySelector('.sidebar__brand i')
+let slideEntire = $('.content__slide')
+let slide = slideEntire.querySelector('.content__slide-list')
+let slideItem = slide.querySelectorAll('.slide-list__item')
+let btnSlideMovie = slideEntire.querySelectorAll('.slide-pick__item')
+let modal = $('#modal')
+let modalBody = modal.querySelector('.modal__body')
+let modalVideoPopup = modalBody.querySelector('.modal-body__video-popup')
+let btnHideModal = modalVideoPopup.querySelector('.btn__hideModal')
+let videoPopup = modalVideoPopup.querySelector('.modal-video-popup__container iframe')
+let btnTrailer = $$('.btn__trailer')
+slide.append(slideItem[0].cloneNode(true))
+slide.prepend(slideItem[slideItem.length - 1].cloneNode(true))
 
 const app = {
-    currentIdx: 0,
+    currentIdx: 1,
+    walk: 0,
+    startX: 0,
+    isDown: false,
+    time1: new Date,
+    time2: new Date,
     // Xử lý sự kiện
     handleEvent() {
-        const _this = this;
+        const _this = this
+
         // Toggle sidebar nhỏ/lớn
         btnMenu.onclick = () => {
             sidebar.classList.toggle('small')
@@ -24,26 +37,13 @@ const app = {
         btnSlideMovie.forEach((ele, idx) => {
             ele.onclick = function () {
                 if (!this.classList.contains('picked')) {
+                    _this.currentIdx = idx + 1
                     _this.removeClassOfBtnSlide()
-                    this.classList.add('picked')
-                    slide.style.transform = 'translateX(-' + 100 * idx + '%)'
-                    currentIdx = idx
+                    slide.classList.add('animation')
+                    slide.style.transform = 'translateX(-' + 100 * _this.currentIdx + '%)'
                 }
             }
         })
-
-        window.onscroll = () => {
-            if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-                header.style.background = 'var(--nav-color)'
-                inputSearch.style.background = 'var(--search-color)'
-                btnSearch.style.background = 'var(--border-color)'
-            }
-            else {
-                header.style.background = 'transparent'
-                inputSearch.style.background = 'transparent'
-                btnSearch.style.background = 'transparent'
-            }
-        }
 
         // Tự động chạy slide sau 5s
         let mySetInterval = setInterval(() => {
@@ -62,22 +62,123 @@ const app = {
             }, 5000)
         }
 
+        // Xem trailer
+        btnTrailer.forEach(ele => {
+            ele.onclick = function () {
+                videoPopup.src = this.getAttribute('data-iframe')
+                modal.classList.add('show')
+                modalVideoPopup.classList.add('show')
+            }
+        })
+
+        // click vào overlay thì ẩn modal
+        window.onclick = e => {
+            if (e.target.classList.contains('modal__body')) {
+                modal.classList.remove('show')
+                modalVideoPopup.classList.remove('show')
+                videoPopup.src = ''
+            }
+        }
+        btnHideModal.onclick = () => {
+            modal.classList.remove('show')
+            modalVideoPopup.classList.remove('show')
+            videoPopup.src = ''
+        }
+
+        // Kéo slide
+        'mousedown touchstart'.split(' ').forEach(ele => {
+            slide.addEventListener(ele, e => {
+                _this.isDown = true
+                _this.time1 = new Date()
+                _this.walk = 0
+                _this.startX = e.clientX - slide.offsetLeft
+                slide.classList.add('animation')
+                slide.classList.add('stop')
+            })
+        })
+        'mouseup touchend'.split(' ').forEach(ele => {
+            slide.addEventListener(ele, () => {
+                _this.isDown = false
+                _this.time2 = new Date()
+                _this.moveSlideWhenDrag(_this.walk)
+                _this.removeClassOfBtnSlide()
+                slide.style.transform = 'translateX(-' + 100 * _this.currentIdx + '%)'
+                _this.loopSlide()
+                slide.classList.remove('stop')
+            })
+        })
+        'mousemove touchmove'.split(' ').forEach(ele => {
+            slide.addEventListener(ele, e => {
+                if (!_this.isDown) {
+                    return
+                } else {
+                    e.preventDefault()
+                    _this.walk = (_this.startX - e.clientX - slide.offsetLeft) / slide.offsetWidth * 100
+                    slide.style.transform = 'translateX(' + (-(100 * _this.currentIdx + _this.walk)) + '%)'
+                }
+            })
+        })
+        'mouseleave touchcancel'.split(' ').forEach(ele => {
+            slide.addEventListener(ele, () => {
+                if (_this.isDown) {
+                    time2 = new Date()
+                    _this.moveSlideWhenDrag(_this.walk)
+                    _this.removeClassOfBtnSlide()
+                    slide.style.transform = 'translateX(-' + 100 * _this.currentIdx + '%)'
+                }
+                _this.isDown = false
+                slide.classList.remove('stop')
+            })
+        })
+    },
+    moveSlideWhenDrag(distance) {
+        if ((distance > 40 && this.currentIdx + 1 < btnSlideMovie.length + 2) || (distance > 0 && this.timeSpan(this.time1, this.time2) < 250)) {
+            this.currentIdx++
+        }
+        if ((distance < -40 && this.currentIdx - 1 > -1) || (distance < 0 && this.timeSpan(this.time1, this.time2) < 250)) {
+            this.currentIdx--
+        }
+        console.log(this.timeSpan(this.time1, this.time2))
     },
     // Tự động chạy slide
     autoSlide() {
         this.currentIdx++
-        if (this.currentIdx > btnSlideMovie.length - 1) {
-            this.currentIdx = 0
-        }
-        this.removeClassOfBtnSlide()
-        btnSlideMovie[this.currentIdx].classList.add('picked')
+        slide.classList.add('animation')
         slide.style.transform = 'translateX(-' + 100 * this.currentIdx + '%)'
+        this.loopSlide()
+        this.removeClassOfBtnSlide()
+    },
+    loopSlide() {
+        slide.addEventListener('transitionend', () => {
+            if (this.currentIdx == btnSlideMovie.length + 1) {
+                this.currentIdx = 1
+                slide.classList.remove('animation')
+                slide.style.transform = 'translateX(-' + 100 * this.currentIdx + '%)'
+            }
+            if (this.currentIdx == 0) {
+                this.currentIdx = btnSlideMovie.length
+                slide.classList.remove('animation')
+                slide.style.transform = 'translateX(-' + 100 * this.currentIdx + '%)'
+            }
+        })
     },
     // xóa class piked ở nút chọn chuyển slide
     removeClassOfBtnSlide() {
-        btnSlideMovie.forEach((ele) => {
+        btnSlideMovie.forEach(ele => {
             ele.classList.remove('picked')
         })
+        let idx = this.currentIdx
+        if (this.currentIdx == 0) {
+            idx = 5
+        } else if (this.currentIdx == 7) {
+            idx = 0
+        } else {
+            idx--
+        }
+        btnSlideMovie[idx].classList.add('picked')
+    },
+    timeSpan(time1, time2) {
+        return time2.getTime() - time1.getTime()
     },
     start() {
         // Xử lý sự kiện
